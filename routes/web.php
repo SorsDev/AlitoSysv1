616f8b97609ee29c06114a50f94e20426ca97191
SorsDev <sorsromerze@gmail.com>
2021-06-16 18:01:21 -0500

    loginAdmin_loginUserv2

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ Route::group(['prefix'=>'admin','middleware'=>['admin:admin']], function(){
     Route::post('login',[AdminController::class, 'store'])->name('admin.login');
 });
 
-
 Route::middleware(['auth:sanctum,admin', 'verified'])->get('/admin/dashboard', function () {
     return view('admin.index');
 })->name('dashboard');
